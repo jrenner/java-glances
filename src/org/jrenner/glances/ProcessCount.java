b05ebdb43eb42155f61f008aa3eb36bf6022ca1c
Jon Renner <rennerjc@gmail.com>
2013-03-11 11:33:20 +0800

    version 0.2

--- src/org/jrenner/glances/ProcessCount.java
+++ src/org/jrenner/glances/ProcessCount.java
@@ -8,14 +8,11 @@ public class ProcessCount {
     public int running;
     public int total;
     public int sleeping;
-    public int disk_sleep;
     // TODO figure out how to handle such things as "disk sleep" and other statuses
 
     public String toString() {
         String text = String.format("ProcessCount: zombie: %d, running: %d, total: %d, " +
             "sleeping: %d", zombie, running, total, sleeping);
-        if (disk_sleep > 0)
-            text += String.format("disk sleep: %d", disk_sleep);
         return text;
     }
 }
